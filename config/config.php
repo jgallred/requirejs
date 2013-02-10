e7acf933785781328b3e0ae5af015979d5e2c404
jgallred <me>
2013-02-09 19:39:46 -0700

    Toying with separate classes

--- config/config.php
+++ config/config.php
@@ -1,21 +1,20 @@
 <?php
 
-return array(    
+return array(
     "use_min_require" => true,
-    
-    "rjs_location" => path("public")."bundles/requirejs/r.js",
-    
+
     "build_profile" => path("public")."js/app/config/app.build.js",
-    
+
     "build_args" => "",
-    
+
     "load_optimized" => false,
-    
+
     "build_dir" => "js/app-build/",
-    
+
     "app_dir" => "js/app/",
-    
+
     "config" => array(
+		//"baseUrl" => "js/app/",
         "paths" => array(
             "haha" => "two"
         )
--- tasks/optimize.php
+++ tasks/optimize.php
@@ -4,61 +4,62 @@
  *
  * @author Jason Allred
  */
-class Requirejs_Optimize_Task 
+class Requirejs_Optimize_Task
 {
+
     /**
      * Run the r.js optimizer using node and the config file build profile. Additionally applies
      * and other args
      * @param array $arguments
      * @return int
      */
-    public function run($arguments) 
+    public function run($arguments)
     {
         if(!Config::has("requirejs::config.rjs_location")) {
             echo "You must specify the location of the r.js file.";
             return -1;
         }
-        
+
         if(!Config::has("requirejs::config.build_profile")) {
             echo "You must specify a build_profile in the bundle config file.";
             return -1;
         }
-        
-        $cmd = "node ".Config::get("requirejs::config.rjs_location")." -o ".Config::get("requirejs::config.build_profile");
-        
+
+        $cmd = "node ".path("public")."bundles/requirejs/r.js -o ".Config::get("requirejs::config.build_profile");
+
         if(Config::has("requirejs::config.build_args")) {
             $cmd .= " ".Config::get("requirejs::config.build_args");
         }
-        
+
         if(count($arguments) > 0) {
             $cmd .= " ".implode(" ", $arguments);
         }
-        
+
         echo "\n$cmd\n";
         passthru($cmd);
         return 0;
     }
-    
+
     /**
-     * 
+     *
      * @param array $arguments
      * @return int
      */
-    public function rjs($arguments) 
+    public function rjs($arguments)
     {
         if(!Config::has("requirejs::config.rjs_location")) {
             echo "You must specify the location of the r.js file.";
             return -1;
         }
-        
-        $cmd = "node ".Config::get("requirejs::config.rjs_location");
-        
+
+        $cmd = "node ".path("public")."bundles/requirejs/r.js";
+
         if(count($arguments) > 0) {
             $cmd .= " ".implode(" ", $arguments);
         }
-        
+
         echo "\n$cmd\n";
         passthru($cmd);
-        return 0;   
+        return 0;
     }
 }
